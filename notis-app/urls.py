aeb9244f0ada7f8dc32849258e0ab54dfb279d38
yamalep-1 <nurulinemil26@gmail.com>
2025-10-04 13:35:20 +0300

    Update urls.py

--- notis-app/urls.py
+++ notis-app/urls.py
@@ -6,3 +6,16 @@ from . import views
 urlpatterns = [
     
 ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'notes', views.NoteViewSet, basename='note')
+
+urlpatterns = [
+    # Этот путь отвечает за все CRUD-операции с заметками:
+    # GET /api/notes/, POST /api/notes/, GET /api/notes/1/, PUT /api/notes/1/, DELETE /api/notes/1/
+    path('', include(router.urls)),
+]
