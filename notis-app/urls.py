3d4c5ce106826d17ce84185719a6ee596aa951f0
yamalep-1 <nurulinemil26@gmail.com>
2025-10-04 13:24:47 +0300

    Create urls.py

---
+++ notis-app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# Этот список пока пуст, но именно здесь мы будем добавлять пути для REST API 
+# на следующем этапе, когда создадим views.py
+urlpatterns = [
+    
+]
